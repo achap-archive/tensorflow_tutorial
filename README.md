265166adc9b994927c90b9fbf270de19a698444f
Ashlyn Chapman <apchapma@ncsu.edu>
2021-09-16 11:50:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+[Tutorial](https://www.youtube.com/playlist?list=PLzMcBGfZo4-mP7qA9cagf68V06sko5otr)
+
+[Dataset Source](https://archive.ics.uci.edu/ml/datasets/Student+Performance)
---
+++ Regression.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv('student/student-mat.csv', sep=';')
+data = data[['G1', 'G2', 'G3', 'studytime', 'failures', 'absences']]
+
+predict = 'G3'
+
+X = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+x_train, y_train, x_test, y_test = sklearn.model_selection.train_test_split(X, y,test_size=0.1)
